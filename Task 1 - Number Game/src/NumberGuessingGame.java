ee95a70b87b8b06506164c2c45134defea6dc93b
Faiz Zaidi <78683533+pikachu0108@users.noreply.github.com>
2024-01-18 19:13:35 +0530

    Update NumberGuessingGame.java

--- Task 1 - Number Game/src/NumberGuessingGame.java
+++ Task 1 - Number Game/src/NumberGuessingGame.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class NumberGuessingGame {
+public class GuessingGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
