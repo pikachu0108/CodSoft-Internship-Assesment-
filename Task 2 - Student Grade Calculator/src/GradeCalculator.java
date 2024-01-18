da7792c77ce3c55a9e4ccd833f27d3c2a89d4c9e
Faiz Zaidi <78683533+pikachu0108@users.noreply.github.com>
2024-01-18 19:14:13 +0530

    Update GradeCalculator.java

--- Task 2 - Student Grade Calculator/src/GradeCalculator.java
+++ Task 2 - Student Grade Calculator/src/GradeCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class GradeCalculator {
+public class GradeCalc {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
